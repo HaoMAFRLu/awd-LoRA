94dbda210d0fb5bea3ea8c438c688729438d9a76
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-07-28 16:58:12 +0200

    tt

--- lowspa_ddp/layer_solver.py
+++ lowspa_ddp/layer_solver.py
@@ -37,6 +37,19 @@ class ADMMSolver():
         """
         Compute the loss term for the model.
         """
+        def _dev(x): 
+            return str(x.device) if torch.is_tensor(x) else f"<non-tensor:{type(x)}>"
+
+        print(
+            "[debug devices]",
+            "current:", torch.cuda.current_device(),
+            "X:", _dev(self.X_with_grad),
+            "L:", _dev(L),
+            "S:", _dev(S),
+            "Y:", _dev(Y),
+            "rho:", _dev(self.rho) if isinstance(self.rho, torch.Tensor) else type(self.rho)
+        )
+
         if self.loss_version == 'v1':
             loss = self.rho/2 * torch.norm(self.X_with_grad - L - S + Y/self.rho, p='fro') ** 2
         elif self.loss_version == 'v2':
