c77f208f6a5b3a2d5bccedd5a6df0742fedf28ba
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-07-28 13:47:38 +0200

    low spa -> cluster

--- lowspa_ddp/layer_solver.py
+++ lowspa_ddp/layer_solver.py
@@ -1,6 +1,4 @@
 import torch
-import torch.distributed as dist
-import torch.nn as nn
 
 from lowspa_ddp.utils import *
 
--- lowspa_ddp/model_register.py
+++ lowspa_ddp/model_register.py
@@ -1,7 +1,6 @@
 """
 """
 import torch
-import torch.nn as nn
 import os, sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
