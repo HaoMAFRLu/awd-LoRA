c67dfc39612ea34fb551fceffab28d5611efb7fa
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-07-31 13:14:48 +0200

    partial eval

--- LowSpa/evaluation_cross_model.py
+++ LowSpa/evaluation_cross_model.py
@@ -82,6 +82,12 @@ def main(model_type: str,
 
     # with '.weight' suffix
     layers = [entry['name'] for entry in cfg_lowspa['layers']]
+
+    exclude_layers = ['transformer.h.0.mlp.c_fc.weight',
+                     'transformer.h.0.mlp.c_proj.weight']
+    
+    layers = [layer for layer in layers if layer not in exclude_layers]
+
     evaluator = CrossEvaluator(model_type=model_type,
                                baseline=model_baseline,
                                lowspa_model=model_lowspa,
@@ -101,7 +107,7 @@ def main(model_type: str,
 if __name__ == "__main__":
     model_type = 'GPT'
     file_baseline = '20250730_221825'
-    file_lowspa = '20250731_084208'
+    file_lowspa = '20250730_215710'
     main(model_type=model_type, 
          file_baseline=file_baseline, 
          file_lowspa=file_lowspa)
