9aebbc28e6da767345fdfcef920d93167c320b97
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-08-04 13:14:29 +0200

    train

--- lowspa_ddp/lr_scheduler.py
+++ lowspa_ddp/lr_scheduler.py
@@ -12,17 +12,27 @@ class GPTScheduler:
         self.lr_decay_iters = lr_decay_iters
         self.min_lr = min_lr
         self.lr = lr
+        self.lr_list = []
     
     def get_lr(self, it) -> float:
         """
         Calculate the learning rate based on the current iteration.
         """
         if it < self.warmup_iters:
-            return self.lr * it / self.warmup_iters
+            _lr = self.lr * (it / self.warmup_iters)
         elif it > self.lr_decay_iters:
-            return self.min_lr
+            _lr = self.min_lr
         
         decay_ratio = (it - self.warmup_iters) / (self.lr_decay_iters - self.warmup_iters)
         assert 0 <= decay_ratio <= 1, "Decay ratio must be between 0 and 1"
         coeff = 0.5 * (1 + math.cos(math.pi * decay_ratio))
-        return self.min_lr + (self.lr - self.min_lr) * coeff
+        _lr = self.min_lr + (self.lr - self.min_lr) * coeff
+
+        self.lr_list.append(_lr)
+        return _lr
+    
+    def get_last_lr(self) -> float:
+        """
+        Get the last calculated learning rate.
+        """
+        return [self.lr_list[-1]]
