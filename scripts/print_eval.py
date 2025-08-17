d5bac47d9391a03b87a7cfac665d1d9599c36ef3
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-08-17 22:48:41 +0200

    train

--- scripts/print_eval.py
+++ scripts/print_eval.py
@@ -168,6 +168,7 @@ if __name__ == "__main__":
     model_type = 'llama_60m'
     files = ['20250814_150324',
              '20250816_004617',
-             '20250816_205604',]
+             '20250816_205604',
+             '20250817_140155',]
     main(model_type=model_type,
          files=files)
