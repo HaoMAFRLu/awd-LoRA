32dce968f2a742ef01fc6b7ba5cdfef098366cb2
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-08-22 08:36:45 +0200

    eval

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -146,7 +146,7 @@ def main(cfg_version: str,
 
 if __name__ == "__main__":
     cfg_version = 'llama_60m'
-    file = '20250819_172224'
+    file = '20250821_135056'
     ex_layers = [
                 #  'layers.0.mlp.down_proj',
                 #  'layers.0.mlp.up_proj',
