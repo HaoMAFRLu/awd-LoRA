aea2e7961a794233ea8984f637c63c22a2e333fb
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-08-22 23:37:02 +0200

    eval

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -146,7 +146,7 @@ def main(cfg_version: str,
 
 if __name__ == "__main__":
     cfg_version = 'llama_60m'
-    file = '20250822_100640'
+    file = '20250822_103123'
     ex_layers = [
                 #  'layers.0.self_attn.o_proj',
                 #  'layers.0.mlp.down_proj',
@@ -155,14 +155,14 @@ if __name__ == "__main__":
                 #  'layers.3.mlp.down_proj',
                 #  'layers.3.self_attn.o_proj',
                 #  'layers.3.self_attn.v_proj',
-                #  'layers.4.self_attn.o_proj',
+                 'layers.4.self_attn.o_proj',
                 #  'layers.4.self_attn.q_proj',
                 #  'layers.4.self_attn.k_proj',
-                #  'layers.4.self_attn.v_proj',
+                 'layers.4.self_attn.v_proj',
                 #  'layers.4.mlp.gate_proj',
                 #  'layers.4.mlp.down_proj',
                 #  'layers.4.mlp.up_proj',
-                #  'layers.5.self_attn.o_proj',
+                 'layers.5.self_attn.o_proj',
                 #  'layers.5.self_attn.v_proj',
                 #  'layers.5.mlp.gate_proj',
                 #  'layers.5.mlp.down_proj',
@@ -170,14 +170,14 @@ if __name__ == "__main__":
                 #  'layers.6.self_attn.o_proj',
                 #  'layers.6.self_attn.v_proj',
                 #  'layers.6.mlp.gate_proj',
-                #  'layers.6.mlp.down_proj',
+                 'layers.6.mlp.down_proj',
                 #  'layers.6.mlp.up_proj',
-                #  'layers.7.self_attn.o_proj',
+                 'layers.7.self_attn.o_proj',
                 #  'layers.7.self_attn.q_proj',
                 #  'layers.7.self_attn.k_proj',
                  'layers.7.self_attn.v_proj',
                 #  'layers.7.mlp.gate_proj',
-                #  'layers.7.mlp.down_proj',
+                 'layers.7.mlp.down_proj',
                 #  'layers.7.mlp.up_proj'
                 ]
     path_folder = os.path.join(root, 'data', 'salad', cfg_version, file)
