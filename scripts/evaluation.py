d5db9254ae59f5999747eb11188326ff6d01bbe2
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-09-15 16:44:28 +0200

    eval

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -146,7 +146,7 @@ def main(cfg_version: str,
 
 if __name__ == "__main__":
     cfg_version = 'llama_60m'
-    file = '20250904_222805'
+    file = '20250905_101501'
     ex_layers = [
                 #  'layers.0.self_attn.o_proj',
                 #  'layers.0.self_attn.v_proj',
