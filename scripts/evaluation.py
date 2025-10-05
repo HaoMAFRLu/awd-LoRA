7885536e8e88b864f8e12bfd8161478316fea558
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-10-05 23:09:22 +0200

    eval

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -167,7 +167,7 @@ def main(cfg_version: str,
 
 if __name__ == "__main__":
     cfg_version = 'llama_60m'
-    file = '20251005_132342'
+    file = '20251005_133102'
     path_folder = os.path.join(root, 'data', 'salad', cfg_version, file)
     main(cfg_version, path_folder, nr_remove=15)
     
