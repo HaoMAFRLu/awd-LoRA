d944950bc7007a9cdeb14a27281ca0784f1545b0
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-10-05 13:50:01 +0200

    train

--- scripts/params.py
+++ scripts/params.py
@@ -62,7 +62,7 @@ attn_o_proj_params = {
     'is_init':       False,  # Whether to initialize
     'iter_max':      1,       # Max ADMM iterations per layer
     'tol':           0.001,   # Convergence tolerance
-    'rate_rank':     0.15,  # Rate of rank reduction for the layer
+    'rate_rank':     0.10,  # Rate of rank reduction for the layer
     'rate_sparsity': 0.05,  # Rate of sparsity for the layer
     'alpha_dict': {
         'init': 1e-7,
@@ -91,7 +91,7 @@ attn_q_proj_params = {
     'is_init':       False,  # Whether to initialize
     'iter_max':      1,       # Max ADMM iterations per layer
     'tol':           0.001,   # Convergence tolerance
-    'rate_rank':     0.15,  # Rate of rank reduction for the layer
+    'rate_rank':     0.10,  # Rate of rank reduction for the layer
     'rate_sparsity': 0.05,  # Rate of sparsity for the layer
     'alpha_dict': {
         'init': 1e-7,
@@ -120,7 +120,7 @@ attn_k_proj_params = {
     'is_init':       False,  # Whether to initialize
     'iter_max':      1,       # Max ADMM iterations per layer
     'tol':           0.001,   # Convergence tolerance
-    'rate_rank':     0.15,  # Rate of rank reduction for the layer
+    'rate_rank':     0.10,  # Rate of rank reduction for the layer
     'rate_sparsity': 0.05,  # Rate of sparsity for the layer
     'alpha_dict': {
         'init': 1e-7,
@@ -149,7 +149,7 @@ attn_v_proj_params = {
     'is_init':       False,  # Whether to initialize
     'iter_max':      1,       # Max ADMM iterations per layer
     'tol':           0.001,   # Convergence tolerance
-    'rate_rank':     0.15,  # Rate of rank reduction for the layer
+    'rate_rank':     0.10,  # Rate of rank reduction for the layer
     'rate_sparsity': 0.05,  # Rate of sparsity for the layer
     'alpha_dict': {
         'init': 1e-7,
@@ -178,7 +178,7 @@ mlp_gate_proj_params = {
     'is_init':       False,  # Whether to initialize
     'iter_max':      1,       # Max ADMM iterations per layer
     'tol':           0.001,   # Convergence tolerance
-    'rate_rank':     0.35,  # Rate of rank reduction for the layer
+    'rate_rank':     0.10,  # Rate of rank reduction for the layer
     'rate_sparsity': 0.05,  # Rate of sparsity for the layer
     'alpha_dict': {
         'init': 1e-7,
@@ -207,7 +207,7 @@ mlp_down_proj_params = {
     'is_init':        False,  # Whether to initialize
     'iter_max':       1,       # Max ADMM iterations per layer
     'tol':            0.001,   # Convergence tolerance
-    'rate_rank':      0.35,  # Rate of rank reduction for the layer
+    'rate_rank':      0.10,  # Rate of rank reduction for the layer
     'rate_sparsity':  0.05,  # Rate of sparsity for the layer
     'alpha_dict': {
         'init': 1e-7,
@@ -236,7 +236,7 @@ mlp_up_proj_params = {
     'is_init':       False,  # Whether to initialize
     'iter_max':      1,       # Max ADMM iterations per layer
     'tol':           0.001,   # Convergence tolerance
-    'rate_rank':     0.35,  # Rate of rank reduction for the layer
+    'rate_rank':     0.10,  # Rate of rank reduction for the layer
     'rate_sparsity': 0.05,  # Rate of sparsity for the layer
     'alpha_dict': {
         'init': 1e-7,
