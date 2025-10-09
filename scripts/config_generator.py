1353d78e4f7b8811ff7e7047c88205a67b695e3f
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-10-10 00:14:58 +0200

    train

--- scripts/config_generator.py
+++ scripts/config_generator.py
@@ -178,7 +178,7 @@ if __name__ == "__main__":
     cfg_llama_350m = dict(
         name='llama_350m',
         seed=42,
-        lr=0.001,
+        lr=0.003,
         gradient='coupled',  # or decoupled
         is_asyn=False,
         is_init=False,
