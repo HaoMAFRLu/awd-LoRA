1303a1c9936e674d432aab7d8622fc196fb59ed6
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-10-14 14:36:28 +0200

    eval

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -250,7 +250,7 @@ if __name__ == "__main__":
     # file = '20251006_223931'
 
     cfg_version = 'llama_350m'
-    file = '20251012_115809'
+    file = '20251012_120507'
 
     rank_cfg = {
         'o_proj': 0.25,
