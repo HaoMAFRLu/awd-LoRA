d00eb90650d853b5e5a712744bc6ef79c677ee80
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-10-24 15:45:55 +0200

    Remove all test files

--- awave/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-.. include:: ../readme.md
-"""
-from .losses import get_loss_f
-from awave.utils.train import Trainer
-from .transform1d import DWT1d
-from .transform2d import DWT2d
--- scripts/quick_test.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-print("Torch version:", torch.version)
-print("CUDA version:", torch.version.cuda)
-print("CUDA available:", torch.cuda.is_available())
-print("Device name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "None")
-print("Number of GPUs:", torch.cuda.device_count())
