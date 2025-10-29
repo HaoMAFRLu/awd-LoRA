bbf78ef2252e0b3190d07f3c76f16bc1c3bdb302
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-10-29 15:38:25 +0100

    batch jobs for ablation studies

--- scripts/train_salad.py
+++ scripts/train_salad.py
@@ -5,6 +5,7 @@ import yaml
 from datetime import datetime
 import shutil
 import transformers
+import argparse
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from salad.utils import *
@@ -17,13 +18,29 @@ torch.backends.cuda.enable_flash_sdp(False)
 
 root = get_parent_path(lvl=1)
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--alpha_rate', type=float, default=None, help='Alpha Rate')
+    parser.add_argument('--beta_rate', type=float, default=None, help='Beta Rate')
+
+    return parser.parse_args()
+
 def main(cfg_version: str, 
          path_cfg: str,
-         path_cfg_model) -> None:
+         path_cfg_model: str,
+         alpha_rate: float,
+         beta_rate: float) -> None:
+    
     # load the config
     with open(path_cfg) as f:
         cfg = yaml.safe_load(f)
     
+    if alpha_rate is not None and beta_rate is not None:
+        for layer in cfg['layers']:
+            layer['params']['alpha_dict']['rate_decay'] = alpha_rate
+            layer['params']['beta_dict']['rate_decay'] = beta_rate 
+
     seed = cfg['seed']
     set_seed(seed)
 
@@ -34,7 +51,12 @@ def main(cfg_version: str,
                     os.path.join(path_folder, 'salad'), 
                     dirs_exist_ok=True, 
                     copy_function=shutil.copy2) 
-    shutil.copy(path_cfg, path_folder)
+    
+    # shutil.copy(path_cfg, path_folder)
+    output_path = os.path.join(path_folder, cfg_version+'.yaml')
+    with open(output_path, 'w', encoding='utf-8') as f:
+        yaml.safe_dump(cfg, f, sort_keys=False, allow_unicode=True)
+
     shutil.copy(path_cfg_model, path_folder)
     
     # get the data loader
@@ -45,7 +67,11 @@ def main(cfg_version: str,
     ddp_trainer.train(path_folder=path_folder)
     
 if __name__ == "__main__":
+    args = parse_args()
+
     cfg_version = 'llama_60m'
     path_cfg = os.path.join(root, 'scripts', 'configs', cfg_version+'.yaml')
     path_cfg_model = os.path.join(root, 'scripts', 'configs', cfg_version+'_model.json')
-    main(cfg_version, path_cfg, path_cfg_model)
+
+
+    main(cfg_version, path_cfg, path_cfg_model, args.alpha_rate, args.beta_rate)
