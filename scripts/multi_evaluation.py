c4b7670b53159efff8d56a5233635fb8811c0338
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-11-03 20:56:17 +0100

    eval 130m

--- scripts/multi_evaluation.py
+++ scripts/multi_evaluation.py
@@ -141,7 +141,7 @@ def main(cfg_version: str,
 
 if __name__ == "__main__":
 
-    cfg_version = 'llama_60m'
+    cfg_version = 'llama_130m'
 
     rank_cfg = {
         'o_proj': 0.25,
@@ -153,7 +153,7 @@ if __name__ == "__main__":
         'up_proj': 0.35
     }
 
-    _path = os.path.join(root, 'data', 'ablation1', cfg_version)
+    _path = os.path.join(root, 'data', 'ablation', cfg_version)
     files = os.listdir(_path)
     for file in files:
         print(f'Processing folder: {file}')
