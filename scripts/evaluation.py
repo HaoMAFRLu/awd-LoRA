88c6993210bd5349af0fda75f6d47fee4404909c
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-11-04 22:17:49 +0100

    eval

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -282,11 +282,11 @@ if __name__ == "__main__":
     # cfg_version = 'llama_130m'
     # file = '20251006_223931'
 
-    cfg_version = 'llama_350m'
-    file = '20251012_120507'
+    # cfg_version = 'llama_350m'
+    # file = '20251012_120507'
 
-    # cfg_version = 'llama_1b'
-    # file = '20251016_233939'
+    cfg_version = 'llama_1b'
+    file = '20251016_233939'
 
     # setting for 350m
     # rank_cfg = {
