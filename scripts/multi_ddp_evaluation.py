e564deabf883e850ee798f6f4f38cb59b8f8e3c7
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-11-05 17:01:32 +0100

    eval

--- scripts/multi_ddp_evaluation.py
+++ scripts/multi_ddp_evaluation.py
@@ -113,7 +113,7 @@ if __name__ == "__main__":
     
     MODEL_TYPE = 'llama_60m'
     FOLDERS = ['salad', 'ablation']
-    gamma_list = [1.0, 0.7, 0.4, 0.1]
+    gamma_list = [1.0, 0.99, 0.98, 0.97]
 
     rank, world_size = ddp_setup()
     files = []
@@ -124,11 +124,11 @@ if __name__ == "__main__":
     files = sorted(files)
     my_files = files[rank::world_size]
 
-    # if rank == 0:
-    #     my_files = ['20251029_162352']
-    #     # my_files = ['20251009_205606']
-    # else:
-    #     my_files = []   
+    if rank == 0:
+        my_files = ['20251029_162352']
+        # my_files = ['20251009_205606']
+    else:
+        my_files = []   
 
     if not my_files:
         print(f"[rank {rank}] No file assigned. Exit.")
