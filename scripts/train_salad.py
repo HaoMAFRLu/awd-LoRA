4d8d812db31776e10adf28d7e33c1c8a66a6a452
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-11-12 14:39:03 +0100

    train

--- scripts/train_salad.py
+++ scripts/train_salad.py
@@ -44,6 +44,7 @@ def main(cfg_version: str,
          beta_rate: float) -> None:
     
     rank, world_size = _init_distributed()
+    print(f'Total world size: {world_size}')
     torch.cuda.set_device(rank % torch.cuda.device_count())
 
     # load the config
