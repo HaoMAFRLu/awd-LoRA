a44a1991a82983d3a59de962e2c555b624f73b87
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-12-09 22:00:28 +0100

    train

--- scripts/config_generator.py
+++ scripts/config_generator.py
@@ -316,7 +316,7 @@ if __name__ == "__main__":
         optimizer_name='AdamW',
         min_lr_ratio=0.1,
         weight_decay=0.0,
-        num_freq=5,
+        num_freq=20,
         seed_for_shuffle=42,
         num_total_iters=11000,
         batch_size=512,
