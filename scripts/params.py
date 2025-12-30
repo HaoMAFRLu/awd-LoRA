bb7e9a578615353f91c28d539f8406dc4c064db9
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2025-12-30 18:08:37 +0100

    tra

--- scripts/params.py
+++ scripts/params.py
@@ -19,7 +19,7 @@ embed_params = {
         'drate': 0.01,
     },
     'rho_dict':            {
-        'rho':           3e-7,
+        'rho':           5e-8,
         'mode':          'fixed',  # 'fixed', 'shape_dependent', 'adaptive'
         'start_epoch':   2,
         'coeff_rho':     0.1,
