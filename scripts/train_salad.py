23f00528d3de87fcaf334a8dcd4675c8084daecb
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2026-02-22 12:28:40 +0000

    train

--- scripts/train_salad.py
+++ scripts/train_salad.py
@@ -53,13 +53,11 @@ def main(cfg_version: str,
          dbeta: float,
          exclude_layers: list=None) -> None:
     
-    
     rank, world_size = _init_distributed()
     
-    if rank == 0:
-        # fk the error 429!!!!!!
-        # fk the error 502!!!!!!
-        hf_login_once()
+    # fk the error 429!!!!!!
+    # fk the error 502!!!!!!
+    hf_login_once()
 
     print(f'[Rank {rank}] initializing...')
     print(f'[Rank {rank}]: Total world size: {world_size}')
